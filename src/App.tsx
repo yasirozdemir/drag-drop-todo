20f236da4722e5e4200ee69ebeb22d7afcee15b2
M Yasir Ozdemir <muhammedyasirozdemircareer@gmail.com>
2023-09-14 09:40:10 +0300

    #13 -> comments

--- src/App.tsx
+++ src/App.tsx
@@ -8,35 +8,38 @@ import Footer from "./components/Footer";
 function App() {
   const [data, setData] = useState<IData>(initialData);
 
-  // source -> the Droppable component where the items is dragged
-  // destination -> the Droppable componenet where the item is dropped
-  // result -> the object that holds all the details about dragging event, it's a part of react-beautiful-dnd package
-  // checking if the item is dropped in a Droppable component
-  // creating a copy of the todos array to ensure that the original state is not mutated directly
-  // source.index -> the index number of the dragged item in the Droppable component where it is dragged
-  // using the first splice we're removing the item from it's starting point
-  // destination.index -> the index number of the dragged item in the Droppable component where it is dropped
-  // using the second splice we're adding the item to where it's dropped
-
   const handleDragEnd = (result: DropResult) => {
+    // result -> the object that holds all the details about dragging event, it's a part of react-beautiful-dnd package
+    // source -> the Droppable List where the items is dragged
+    // destination -> the Droppable List where the item is dropped
     const { source, destination } = result;
 
-    // out of DroppableLists
+    // checking if the item is dropped in a Droppable List
     if (!destination) return;
 
     const srcId = source.droppableId;
+    // source.droppableId -> the id of the Droppable List that Draggable List Item taken from
     const srcIndex = source.index;
+    // source.index -> the index number of the dragged item in the Droppable List where it is dragged
     const destId = destination.droppableId;
+    // destination.droppableId -> the id of the Droppable List that Draggable List Item dropped to
     const destIndex = destination.index;
+    // destination.index -> the index number of the dragged item in the Droppable List where it is dropped
 
     // didn't move
     if (srcId === destId && srcIndex === destIndex) return;
 
     const sourceList = data[srcId];
+    // source list -> the array of all the Draggable List Items in the source Droppable List
     const destinationList = data[destId];
+    // source list -> the array of all the Draggable List Items in the destiantion Droppable List
 
+    // using splice removing the Draggable List Item from where it is dragged
     const [item] = sourceList.splice(srcIndex, 1);
+    // item -> the Draggable Item that dragged and dropped
+    // using splice to append the Draggable List Item to wher it is dropped
     destinationList.splice(destIndex, 0, item);
+    // updating the state with regenerated arrays
     setData({
       ...data,
       [srcId]: sourceList,
